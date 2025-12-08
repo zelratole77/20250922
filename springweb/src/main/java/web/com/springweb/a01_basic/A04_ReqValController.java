2d0497c8861bb8cf4978771d0ea250b9a116d467
humantech <humantech@host.docker.internal>
2025-12-08 13:47:33 +0900

    111

---
+++ springweb/src/main/java/web/com/springweb/a01_basic/A04_ReqValController.java
@@ -0,0 +1,13 @@
+package web.com.springweb.a01_basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class A04_ReqValController {
+	// http://localhost:5050/req10_one
+	@GetMapping("req10_one")
+	public String getShow() {
+		return "a02_req\\a01_req_one";  // a01_req_one.jsp
+	}
+}
