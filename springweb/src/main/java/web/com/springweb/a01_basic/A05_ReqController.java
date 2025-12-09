7fcf4d04f4808c71ed62745865041615ae56fae4
humantech <humantech@host.docker.internal>
2025-12-09 10:25:22 +0900

    111

---
+++ springweb/src/main/java/web/com/springweb/a01_basic/A05_ReqController.java
@@ -0,0 +1,32 @@
+package web.com.springweb.a01_basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class A05_ReqController {
+	// get/post방식 둘다 처리하는 요청 처리 메서드 선언
+	// http://localhost:5050/reqGetPost
+	@RequestMapping("reqGetPost")
+	public String reqGetPost() {
+		System.out.println("요청처리 get/post 둘다 한꺼번에 처리");
+		return "a02_req/a04_requestGetPost";
+	}
+	// http://localhost:5050/reqGetPost2
+	// a02_req/a05_requestGetPost.jsp 처리 해주세요..
+	@RequestMapping("reqGetPost2")
+	public String reqGetPost2() {
+		return "a02_req/a05_requestGetPost";
+	}
+	// 요청값을 받을 때, 요청값이 없을 처리되는 메서드 선언..
+	// http://localhost:5050/reqParamOpt  	
+	// http://localhost:5050/reqParamOpt?product=사과
+	// 요청값이 없을 때나 있을 때, 둘다 처리.. 요청값이 없으면 공백으로 처리..
+	@RequestMapping("reqParamOpt")
+	public String reqParamOpt(@RequestParam(name = "product", defaultValue = "") String name ) {
+		System.out.println("구매할 물건 :"+name);
+		return "a02_req/a06_reqParamOpt";
+	}	
+	
+}
