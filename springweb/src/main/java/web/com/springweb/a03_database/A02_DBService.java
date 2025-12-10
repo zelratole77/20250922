a0071a720918b28ef0557c043427fa936a96405a
humantech <humantech@host.docker.internal>
2025-12-10 10:38:22 +0900

    1111

---
+++ springweb/src/main/java/web/com/springweb/a03_database/A02_DBService.java
@@ -0,0 +1,5 @@
+package web.com.springweb.a03_database;
+
+public class A02_DBService {
+
+}
---
+++ springweb/src/main/java/web/com/springweb/a03_database/A03_DBDao.java
@@ -0,0 +1,8 @@
+package web.com.springweb.a03_database;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface A03_DBDao {
+
+}
