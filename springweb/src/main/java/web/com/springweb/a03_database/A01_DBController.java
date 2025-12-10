0d901bbd1c5b644c5c9a918b0f81d7d12da2ebcf
humantech <humantech@host.docker.internal>
2025-12-10 11:00:37 +0900

    111

--- springweb/src/main/java/web/com/springweb/a03_database/A01_DBController.java
+++ springweb/src/main/java/web/com/springweb/a03_database/A01_DBController.java
@@ -1,5 +1,18 @@
 package web.com.springweb.a03_database;
 
-public class A01_DBController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class A01_DBController {
+	@Autowired(required=false)
+	private A02_DBService service;
+	
+	// http://localhost:5050/dbexp01
+	@GetMapping("dbexp01")
+	public String dbexp01() {
+		return "a04_dbexp/a01_exp01";
+	}
+	
 }
--- springweb/src/main/java/web/com/springweb/a03_database/A02_DBService.java
+++ springweb/src/main/java/web/com/springweb/a03_database/A02_DBService.java
@@ -1,5 +1,13 @@
 package web.com.springweb.a03_database;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class A02_DBService {
 
+	// 객체끼리 자동으로 연관관계롤 1:1로 할당 처리..
+	@Autowired(required=false)
+	private A03_DBDao dao;
+	
 }
