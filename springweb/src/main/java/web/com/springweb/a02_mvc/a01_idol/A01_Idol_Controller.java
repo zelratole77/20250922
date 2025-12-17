b379eba9609c6c4b68f92a20330784829790c312
humantech <humantech@host.docker.internal>
2025-12-17 11:12:44 +0900

    111

---
+++ springweb/src/main/java/web/com/springweb/a02_mvc/a01_idol/A01_Idol_Controller.java
@@ -0,0 +1,8 @@
+package web.com.springweb.a02_mvc.a01_idol;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class A01_Idol_Controller {
+
+}
---
+++ springweb/src/main/java/web/com/springweb/a02_mvc/a01_idol/A02_Idol_Service.java
@@ -0,0 +1,8 @@
+package web.com.springweb.a02_mvc.a01_idol;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class A02_Idol_Service {
+
+}
