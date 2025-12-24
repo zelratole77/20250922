e62dc7719675ddbe8b9251a2b6af02b8cf62c7ec
humantech <humantech@host.docker.internal>
2025-12-24 10:50:23 +0900

    111

--- springweb/src/main/java/a01_diexp/Config12.java
+++ springweb/src/main/java/a01_diexp/Config12.java
@@ -3,6 +3,9 @@ package a01_diexp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import a01_diexp.vo.Student;
+import a01_diexp.vo.StudentCard;
+
 @Configuration
 public class Config12 {
 //컨테이너를 만든 스프링을 웹에서 계속 수행 작업을 하는 것을
@@ -12,6 +15,24 @@ public class Config12 {
 	@Bean    // 기본적으로 메서드 이름으로 bean이름이 default 설정이 된다.
 	public Object obj() {
 		return new Object();
+	}
+	// 컨테이너 안에 bean 객체 생성
+	@Bean    
+	public StudentCard studentCard() {
+		return new StudentCard("QA244211");
+	}
+	
+	@Bean
+	public Student student() {
+		
+		return new Student("홍길동 학생");
+	}
+	// 컨터이너 안에서 객체안에 객체가 할당되는 내용..
+	@Bean
+	public Student cardStudent() {
+		Student stu = new Student("마길동");
+		stu.setCard(new StudentCard("QB251224"));
+		return stu;
 	}
 	
 }
--- springweb/src/main/java/a01_diexp/Diexp12.java
+++ springweb/src/main/java/a01_diexp/Diexp12.java
@@ -2,6 +2,9 @@ package a01_diexp;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import a01_diexp.vo.Student;
+import a01_diexp.vo.StudentCard;
+
 public class Diexp12 {
 
 	public static void main(String[] args) {
@@ -9,8 +12,11 @@ public class Diexp12 {
 		// 코드 처리..
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Config12.class);
 		// 1. 컨테이너 안에 있는 객체를 호출해서 메모리에 로딩.
-		Object ob = ctx.getBean("obj"); // Bean으로 설정된 이름으로 객체를 호출..
+		Student ob = ctx.getBean("cardStudent", Student.class); // Bean으로 설정된 이름으로 객체를 호출..
 		System.out.println("컨테이너 안에 있는 객체:"+ob);
+		System.out.println(ob.getName());
+		StudentCard cd = ob.getCard();
+		System.out.println("객체 안에 있는 카드 번호:"+cd.getCardNumber());
 		// 2. 컨테이너 처리하는 메모리 해재
 		ctx.close();
 		
