363f1f68a967f16fae9629b3c0fd00fa4daa4145
humantech <humantech@host.docker.internal>
2025-12-29 11:30:49 +0900

    111

--- springweb/src/main/java/web/com/springweb/a04_file/A01_FileController.java
+++ springweb/src/main/java/web/com/springweb/a04_file/A01_FileController.java
@@ -2,6 +2,7 @@ package web.com.springweb.a04_file;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,9 +21,10 @@ public class A01_FileController {
 	}
 	// http://localhost:5050/upload
 	@PostMapping("upload")
-	public String uploadPost(@RequestParam("report") MultipartFile file) {
+	public String uploadPost(@RequestParam("report") MultipartFile file, Model d) {
 		System.out.println("업로드된 파일:"+file.getOriginalFilename());
 		
+		d.addAttribute("msg", service.fileUpload(file));
 		
 		return "a07_file\\a01_fileUpload";
 	}
