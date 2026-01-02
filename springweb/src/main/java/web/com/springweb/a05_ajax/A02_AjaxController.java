ecefa4b5171d98fca7148d9225513e7668b84cbe
humantech <zelratole@gmail.com>
2026-01-02 12:44:05 +0900

    1111

--- springweb/src/main/java/web/com/springweb/a05_ajax/A02_AjaxController.java
+++ springweb/src/main/java/web/com/springweb/a05_ajax/A02_AjaxController.java
@@ -2,6 +2,8 @@ package web.com.springweb.a05_ajax;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class A02_AjaxController {
@@ -11,4 +13,12 @@ public class A02_AjaxController {
 	public String callReqInit() {
 		return "a08_ajax\\a02_ajaxReqView";
 	}
+	// http://localhost:5050/callReqAjax01?name=홍길동
+	@GetMapping("callReqAjax01")
+	@ResponseBody
+	public String callReqAjax01(@RequestParam("name") String name) {
+		String msg=name+" 안녕하세요!!";
+		return msg;
+	}
+	
 }
