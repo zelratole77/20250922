64fd0bdc5cd5938f6d45503355e501d322750499
humantech <zelratole@gmail.com>
2026-01-02 15:48:18 +0900

    111

--- springweb/src/main/java/web/com/springweb/a05_ajax/A03_AjaxDBController.java
+++ springweb/src/main/java/web/com/springweb/a05_ajax/A03_AjaxDBController.java
@@ -1,8 +1,22 @@
 package web.com.springweb.a05_ajax;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class A03_AjaxDBController {
+	
+	@Autowired(required=false)
+	private A04_AjaxDBService service;
+	
+	// http://localhost:5050/deptAjax?deptno=10
+	@GetMapping("deptAjax")
+	public ResponseEntity<?> deptAjax(@RequestParam("deptno") int deptno){
+		
+		return ResponseEntity.ok(service.getDept(deptno));
+	}
 
 }
--- springweb/src/main/java/web/com/springweb/a05_ajax/A04_AjaxDBService.java
+++ springweb/src/main/java/web/com/springweb/a05_ajax/A04_AjaxDBService.java
@@ -1,5 +1,16 @@
 package web.com.springweb.a05_ajax;
 
-public class A04_AjaxDBService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import web.com.springweb.z01_dto.Dept;
+
+@Service
+public class A04_AjaxDBService {
+	@Autowired(required=false)
+	private A05_AjaxDBDao dao;
+	
+	public Dept getDept(int deptno) {
+		return dao.getDept(deptno);
+	}
 }
