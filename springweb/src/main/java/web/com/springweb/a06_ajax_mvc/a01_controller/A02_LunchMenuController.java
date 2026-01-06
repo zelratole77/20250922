7646deac08aa19d280ace3941177024972fe0309
humantech <zelratole@gmail.com>
2026-01-06 15:51:25 +0900

    1111

--- springweb/src/main/java/web/com/springweb/a06_ajax_mvc/a01_controller/A02_LunchMenuController.java
+++ springweb/src/main/java/web/com/springweb/a06_ajax_mvc/a01_controller/A02_LunchMenuController.java
@@ -3,8 +3,11 @@ package web.com.springweb.a06_ajax_mvc.a01_controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -41,5 +44,17 @@ public class A02_LunchMenuController {
 	public ResponseEntity<?> getLunchMenu(@RequestParam("menuId") int menuId){
 		return ResponseEntity.ok(service.getLunchMenu(menuId));
 	}			
-			
+	// 	http://localhost:5050/updateLunchMenu
+	@PutMapping("updateLunchMenu")
+	@ResponseBody
+	public String updateLunchMenu(@RequestBody LunchMenu upt ) { // json 문자열을 요청값으로 처리할 때,
+		return service.updateLunchMenu(upt);
+	}
+	
+	//  http://localhost:5050/deleteLunchMenu
+	@DeleteMapping("deleteLunchMenu")
+	@ResponseBody
+	public String deleteLunchMenu(@RequestParam("menuId") int menuId) { // json 문자열을 요청값으로 처리할 때,
+		return service.deleteLunchMenu(menuId);
+	}				
 }
