ee8ccdced9124e43cd32d0ca5bdc77cccfce3f3a
humantech <zelratole@gmail.com>
2026-01-08 09:03:05 +0900

    1111

--- springweb/database/z02_test.sql
+++ springweb/database/z02_test.sql
@@ -1,24 +1,45 @@
--- 1. 단일 일정 (2월 3일)
-INSERT INTO calendar VALUES(cal_seq.nextval, '팀 주간 회의', '2026-02-03T10:00:00', '2026-02-03T12:00:00', 
-    '#3357FF', '#FFFFFF', 0, '', '김철수', '프로젝트 진행 상황 공유');
 
--- 2. 며칠간 이어지는 프로젝트 일정 (2월 9일 ~ 11일)
-INSERT INTO calendar VALUES(cal_seq.nextval, '기획안 집중 검토', '2026-02-09', '2026-02-12', 
-    '#FF5733', '#FFFFFF', 1, 'http://docs.google.com/plan', '이영희', '상반기 신규 서비스 기획안 확정');
-
--- 3. 오후 시간대 짧은 미팅 (2월 14일)
-INSERT INTO calendar VALUES(cal_seq.nextval, '발렌타인 이벤트 체크', '2026-02-14T14:00:00', '2026-02-14T15:30:00', 
-    '#FF33A1', '#000000', 0, '', '박지민', '매장 디스플레이 및 재고 확인');
-
--- 4. 종일 일정 - 연차 휴가 (2월 20일)
-INSERT INTO calendar VALUES(cal_seq.nextval, '개인 연차', '2026-02-20', '2026-02-21', 
-    '#8E44AD', '#FFFFFF', 1, '', '홍길동', '개인 용무로 인한 휴가');
-
--- 5. 외부 교육 참석 (2월 24일 ~ 25일)
-INSERT INTO calendar VALUES(cal_seq.nextval, 'SQL 성능 최적화 교육', '2026-02-24T09:00:00', '2026-02-25T18:00:00', 
-    '#F1C40F', '#000000', 0, 'http://edu.center.com', '최대리', '데이터베이스 튜닝 실무 과정');
-
--- 변경사항 반영
-COMMIT;
-
-SELECT * FROM calendar;
+			
+create table chart01(
+				no number,
+				type varchar2(50),
+				data number,
+				label varchar2(50)
+			);
+			
+			create sequence chart01_seq;
+			insert into chart01 values(chart01_seq.nextval,'area',50,'1월');
+			insert into chart01 values(chart01_seq.nextval,'area',70,'2월');
+			insert into chart01 values(chart01_seq.nextval,'area',70,'3월');
+			insert into chart01 values(chart01_seq.nextval,'area',80,'4월');
+			insert into chart01 values(chart01_seq.nextval,'area',90,'5월');
+			insert into chart01 values(chart01_seq.nextval,'area',100,'6월');
+			
+			insert into chart01 values(chart01_seq.nextval,'area',80,'7월');
+			insert into chart01 values(chart01_seq.nextval,'area',110,'8월');
+			insert into chart01 values(chart01_seq.nextval,'area',120,'9월');
+			insert into chart01 values(chart01_seq.nextval,'area',90,'10월');
+			insert into chart01 values(chart01_seq.nextval,'area',100,'11월');
+			insert into chart01 values(chart01_seq.nextval,'area',110,'12월');
+			
+			insert into chart01 values(chart01_seq.nextval,'bar',45,'1월');
+			insert into chart01 values(chart01_seq.nextval,'bar',53,'2월');
+			insert into chart01 values(chart01_seq.nextval,'bar',62,'3월');
+			insert into chart01 values(chart01_seq.nextval,'bar',78,'4월');
+			insert into chart01 values(chart01_seq.nextval,'bar',98,'5월');
+			insert into chart01 values(chart01_seq.nextval,'bar',149,'6월');
+			insert into chart01 values(chart01_seq.nextval,'bar',109,'7월');
+			insert into chart01 values(chart01_seq.nextval,'bar',119,'8월');
+			insert into chart01 values(chart01_seq.nextval,'bar',129,'9월');
+			insert into chart01 values(chart01_seq.nextval,'bar',119,'10월');
+			insert into chart01 values(chart01_seq.nextval,'bar',109,'11월');
+			insert into chart01 values(chart01_seq.nextval,'bar',298,'12월');
+			
+			insert into chart01 values(chart01_seq.nextval,'pie',30,'직접방문');
+			insert into chart01 values(chart01_seq.nextval,'pie',50,'추천방문');
+			insert into chart01 values(chart01_seq.nextval,'pie',20,'SNS통한방문');
+			
+			
+			
+			
+			
