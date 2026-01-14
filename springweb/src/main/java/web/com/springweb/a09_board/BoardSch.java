c3664445c33bfe8717f4fbfe7e6a45ec485521fd
humantech <zelratole@gmail.com>
2026-01-14 14:26:46 +0900

    1111

--- springweb/src/main/java/web/com/springweb/a09_board/BoardSch.java
+++ springweb/src/main/java/web/com/springweb/a09_board/BoardSch.java
@@ -2,5 +2,107 @@ package web.com.springweb.a09_board;
 
 // a09_board.BoardSch 페이징 처리 및 검색을 위한 DTO
 public class BoardSch {
+	// 기본 검색 조건
+	private String subject;
+	private String writer;
+	// 페이징 처리를 위한 속성
+	private int count; // 건색된 데이터의 총건수.
+	private int pageSize; // 한번에 보일 데이터 건수[요청값]
+	private int curPage; // 클릭한 현재 페이지 번호[요청값]
+	private int pageCount; // 전체 페이지 건수[계산 알고리즘]
+	// DB에 조건으로 넘길 속성
+	private int start;  //시작번호 [계산 알고리즘]
+	private int end; 	// 마지막번호 [계산 알고리즘]
+	
+	// 페이지 블럭을 위한 속성..
+	private int blockSize; // 페이지 블럭의 크기[설정]
+	private int startBlock; // 페이지 블럭의 시작번호 [계산알고리즘]
+	private int endBlock;   // 페이지 블럭의 마지막번호 [계산 알고리즘]
+	public BoardSch() {
+		// TODO Auto-generated constructor stub
+	}
+	public BoardSch(String subject, String writer, int count, int pageSize, int curPage, int pageCount, int start,
+			int end, int blockSize, int startBlock, int endBlock) {
+		this.subject = subject;
+		this.writer = writer;
+		this.count = count;
+		this.pageSize = pageSize;
+		this.curPage = curPage;
+		this.pageCount = pageCount;
+		this.start = start;
+		this.end = end;
+		this.blockSize = blockSize;
+		this.startBlock = startBlock;
+		this.endBlock = endBlock;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public int getPageSize() {
+		return pageSize;
+	}
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+	public int getCurPage() {
+		return curPage;
+	}
+	public void setCurPage(int curPage) {
+		this.curPage = curPage;
+	}
+	public int getPageCount() {
+		return pageCount;
+	}
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+	}
+	public int getStart() {
+		return start;
+	}
+	public void setStart(int start) {
+		this.start = start;
+	}
+	public int getEnd() {
+		return end;
+	}
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	public int getBlockSize() {
+		return blockSize;
+	}
+	public void setBlockSize(int blockSize) {
+		this.blockSize = blockSize;
+	}
+	public int getStartBlock() {
+		return startBlock;
+	}
+	public void setStartBlock(int startBlock) {
+		this.startBlock = startBlock;
+	}
+	public int getEndBlock() {
+		return endBlock;
+	}
+	public void setEndBlock(int endBlock) {
+		this.endBlock = endBlock;
+	}
 
+	
+	
+	
 }
