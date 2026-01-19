883a05d2261d56f00134ed8e8ec47c9b3cc2fa13
humantech <zelratole@gmail.com>
2026-01-19 10:04:57 +0900

    1111

---
+++ springweb/src/main/java/web/com/springweb/a11_chatting/A01_ChattingHandler.java
@@ -0,0 +1,9 @@
+package web.com.springweb.a11_chatting;
+
+import org.springframework.stereotype.Component;
+
+// 컨테이너에서 객체로 사용하기 위한 설정..
+@Component("chatHandler")
+public class A01_ChattingHandler {
+
+}
